ff0a17b71e184dcaf935ea64b1deb74a1018347d
dlskfo306 <dlskfo306@gmail.com>
2021-06-22 09:25:36 +0900

    restful commit

--- src/main/java/edu/bit/ex/controller/RestBoardController.java
+++ src/main/java/edu/bit/ex/controller/RestBoardController.java
@@ -18,6 +18,13 @@ import lombok.extern.log4j.Log4j;
 //@RestController라는 어노테이션을 추가해서 
 //해당 Controller의 모든 메서드의 리턴타입을 기존과 다르게 처리한다는 것을 명시
 
+//하나의 URI가 하나의 고유한 리소스를 대표하도록 설계된 개념
+
+//http://localhost/spring02/list?bno=1 ==> url+파라미터
+//http://localhost/spring02/list/1 ==> url
+//RestController은 스프링 4.0부터 지원
+//@Controller, @RestController 차이점은 메서드가 종료되면 화면전환의 유무
+
 @Log4j
 @RestController
 @RequestMapping("/restful/*")
@@ -25,15 +32,38 @@ public class RestBoardController {
 
     @Autowired
     private BoardService boardService;
-
-   
-    //1. list(처음 진입 화면이므로 화면이 깜빡여도
-    //상관없어서 @Controller방식으로 접근
-    //- view(화면)을 리턴
+    
+    // 1. list(처음 진입 화면이므로 화면이 깜박여도 상관없으므로 @Controller방식으로 접근 - veiw(화면)를 리턴
     @GetMapping("/board")
     public ModelAndView list( ModelAndView mav) {
-       mav.setViewName("restful/board ..");
+       mav.setViewName("rest/rest_list"); //restful/rest_list.jsp
        mav.addObject("list", boardService.getList());
+       
+       return mav;
+    }
+    
+    
+    /*
+    //위에꺼랑 이거랑 똑같은애임.
+    //근데 위에 @RestController가 있으면 이게 안먹힘.
+    //문법을 완전히 무시하고 단순히 string으로 리턴해버림
+    @GetMapping("/board")
+    public String list(Model model) {
+      
+       model.addAttribute("list", boardService.getList());
+       
+       return "restful/rest_list";
+    }
+    */
+    
+    //조회
+    @GetMapping("/board/{bid}")
+    public ModelAndView rest_content_view(BoardVO boardVO,ModelAndView mav) {
+       
+       log.info("rest_content_view");
+       
+       mav.setViewName("rest/rest_content_view");  
+       mav.addObject("content_view", boardService.read(boardVO.getBid()));
        
        return mav;
     }
